9b9bef58e63eaee193e5da5e64fc1d549bc3c1b0
tairasoul <100938045+tairasoul@users.noreply.github.com>
2025-09-28 11:13:47 +0200

    hopefully allow for catching crashes

--- main.cs
+++ main.cs
@@ -1,8 +1,22 @@
+using System.Diagnostics;
 using yahd2mm;
 
 if (args.Length == 1 && args[0].StartsWith("nxm://")) {
   await EntryPoint.RunHandler(args[0]);
 }
 else {
-  EntryPoint.RunMain();
+  try
+  {
+    EntryPoint.RunMain();
+  }
+  catch (Exception ex) {
+    string baseText = "yahd2mm has crashed, provide this file alongside an issue at https://github.com/tairasoul/yahd2mm/issues\n";
+    baseText += ex.ToStringDemystified();
+    if (!Directory.Exists(Path.Join(ModManager.yahd2mm_basepath, "crashes"))) {
+      Directory.CreateDirectory(Path.Join(ModManager.yahd2mm_basepath, "crashes"));
+      DateTimeOffset baseOffset = new(DateTime.Now);
+      File.WriteAllText(Path.Join(ModManager.yahd2mm_basepath, "crashes", $"{baseOffset:dd-MM-yyyy-HH-mm-ss}"), baseText);
+      EntryPoint.OpenFile(Path.Join(ModManager.yahd2mm_basepath, "crashes", $"{baseOffset:dd-MM-yyyy-HH-mm-ss}"));
+    }
+  }
 }
